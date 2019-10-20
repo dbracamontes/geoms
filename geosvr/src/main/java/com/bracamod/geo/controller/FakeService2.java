313cfa8802ab10ff84e6ae727e53cfccb709ada2
Daniel Bracamontes <daniel.bracamontes@oracle.com>
2019-10-20 12:07:58 -0500

    AOP adding join points

--- geosvr/src/main/java/com/bracamod/geo/controller/FakeService2.java
+++ geosvr/src/main/java/com/bracamod/geo/controller/FakeService2.java
@@ -5,15 +5,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.bracamod.geo.entity.State;
 
+@RestController
 @RequestMapping(value = "/fake2", produces=MediaType.APPLICATION_JSON_VALUE)
 public class FakeService2 {
 	
 	@GetMapping("/fakeMethod")
 	@ResponseBody
 	public ResponseEntity<State> findStateById() throws Exception {
+		
 		throw new Exception("Fake service 2 exception ");
 	}
 
